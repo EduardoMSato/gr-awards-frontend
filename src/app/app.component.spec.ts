af69af2d1913d287d41dde40021d5344e6d18554
esato <eduardo.m.satog@gmail.com>
2026-02-06 01:20:38 -0300

    chore: add tailwind css and primeng with lara theme

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,18 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gr-awards-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('gr-awards-frontend');
-  });
-
-  it('should render title', () => {
+  it('should render router outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'gr-awards-frontend app is running!'
-    );
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
